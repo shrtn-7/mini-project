917fe850ff3bd7da551b33c86a4ec323483dcae8
shrtn-7 <shreetansangisetty@gmail.com>
2025-02-10 23:06:57 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import NavBar from './components/NavBar.jsx'
+import './App.css'
+import Treatment from './components/Treatment.jsx'
+import Overview from './components/Overview.jsx'
+import Footer from './components/Footer.jsx'
+import Contact from './components/Contact.jsx'
+
+function App() {
+
+  return (
+    <>
+      <NavBar/>
+      <Overview/>
+      <Treatment/>
+      <Contact/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Treatment.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import TreatmentCard from './TreatmentCard'
+
+const Treatment = () => {
+  return (
+    <>
+        <div className="container mx-auto px-2">
+            <h2 className='my-10 text-3xl font-bold text-center'>Our Treatments</h2>
+            <div className="flex flex-wrap justify-center gap-4">
+                <TreatmentCard image="https://www.devtopics.com/wp-content/uploads/2023/01/React-icon.svg_.png" title="Diabetes"/>
+                <TreatmentCard image="https://www.devtopics.com/wp-content/uploads/2023/01/React-icon.svg_.png" title="Diabetes"/>
+                <TreatmentCard image="https://www.devtopics.com/wp-content/uploads/2023/01/React-icon.svg_.png" title="Diabetes"/>
+                <TreatmentCard image="https://www.devtopics.com/wp-content/uploads/2023/01/React-icon.svg_.png" title="Diabetes"/>
+                <TreatmentCard image="https://www.devtopics.com/wp-content/uploads/2023/01/React-icon.svg_.png" title="Diabetes"/>
+                <TreatmentCard image="https://www.devtopics.com/wp-content/uploads/2023/01/React-icon.svg_.png" title="Diabetes"/>
+                <TreatmentCard image="https://www.devtopics.com/wp-content/uploads/2023/01/React-icon.svg_.png" title="Diabetes"/>
+                <TreatmentCard image="https://www.devtopics.com/wp-content/uploads/2023/01/React-icon.svg_.png" title="Diabetes"/>
+                <TreatmentCard image="https://www.devtopics.com/wp-content/uploads/2023/01/React-icon.svg_.png" title="Diabetes"/>
+                <TreatmentCard image="https://www.devtopics.com/wp-content/uploads/2023/01/React-icon.svg_.png" title="Diabetes"/>
+                <TreatmentCard image="https://www.devtopics.com/wp-content/uploads/2023/01/React-icon.svg_.png" title="Diabetes"/>
+                <TreatmentCard image="https://www.devtopics.com/wp-content/uploads/2023/01/React-icon.svg_.png" title="Diabetes"/>
+                <TreatmentCard image="https://www.devtopics.com/wp-content/uploads/2023/01/React-icon.svg_.png" title="Diabetes"/>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Treatment
